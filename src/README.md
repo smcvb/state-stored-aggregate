b1412cc24af6a43c9b3bd6775868a45159a6fa44
Steven van Beelen <steven.vanbeelen@axoniq.io>
2019-08-14 16:15:28 +0200

    Initial commit
    Initial commit containing the spring-boot project to show how a
     state-stored aggregate might work

---
+++ src/README.md
@@ -0,0 +1,4 @@
+# State-Stored Aggregate
+
+This sample project aims to show what a State-Stored Aggregate could look like in an [Axon](https://www.axoniq.io) based application.
+The approach is based on what's provided in Axon's [Reference Guide](https://docs.axoniq.io/reference-guide/implementing-domain-logic/command-handling/state-stored-aggregates) on the matter.  
---
+++ src/main/java/com/example/axon/statestoredaggregate/StateStoredAggregateApplication.java
@@ -0,0 +1,13 @@
+package com.example.axon.statestoredaggregate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateStoredAggregateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StateStoredAggregateApplication.class, args);
+	}
+
+}
